3bb23e85b9cecc1208a4f5efd5a91b2a763d27f1
evelyn <evelyn.evangelista@edu.senai.br>
2026-09-20 13:38:39 -0300

    exerciocios-js-moderno

---
+++ exercicio1.js
@@ -0,0 +1,8 @@
+const aluno = {
+  nome: "Ana",
+  idade: 17,
+  curso: "Desenvolvimento de Sistemas"
+};
+
+const {nome, idade, curso} = aluno;
+console.log(nome, "-", idade, "-", curso);
---
+++ exercicio2.js
@@ -0,0 +1,9 @@
+const produto = {
+  nome: "Teclado",
+  preco: 150,
+  marca: "Logitech"
+};
+
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(produto.marca);
